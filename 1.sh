dfeebf0ca5e8c2d4b9e5c7397c80279cff207feb
Michael Halpin <halpinm2010@gmail.com>
2022-03-21 17:37:10 +0000

    .

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
-echo "Install all packages, make a partition."
+echo "Install all packages, make a partition, and make a filesystem on it."
 export LFS=/mnt/lfs
 mkdir -pv $LFS
 echo "mount the filesystem now"
