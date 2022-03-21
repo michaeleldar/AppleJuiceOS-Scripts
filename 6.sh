c2d0515c09dc6300714a543101d5524843af86d5
Michael Halpin <halpinm2010@gmail.com>
2022-03-21 18:24:26 +0000

    .

--- 6.sh
+++ 6.sh
@@ -3,5 +3,8 @@ echo "set +h" > ~/.bash_profile
 echo "umask 022" >> ~/.bash_profile
 echo "LFS=/mnt/lfs" >> ~/.bash_profile
 echo "LC_ALL=POSIX" >> ~/.bash_profile
-LFS_TGT=$(uname -m)-lfs-linux-gnu PATH=/usr/bin
-if [ ! -L /bin ]; then PATH=/bin:$PATH; fi PATH=$LFS/tools/bin:$PATH CONFIG_SITE=$LFS/usr/share/config.site export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+echo "LFS_TGT=$(uname -m)-lfs-linux-gnu PATH=/usr/bin" >> ~/.bash_profile
+echo "if [ ! -L /bin ]; then PATH=/bin:$PATH; fi" >> ~/.bash_profile 
+echo "PATH=$LFS/tools/bin:$PATH" >> ~/.bash_profile
+echo "CONFIG_SITE=$LFS/usr/share/config.site" >> ~/.bash_profile
+echo "export LFS LC_ALL LFS_TGT PATH CONFIG_SITE" >> ~/.bash_profile
